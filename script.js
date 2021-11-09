e01a614f2742bf90c11a54a85a6da0cc31466311
Roman Evgenov <Roman Evgenov>
2021-11-09 19:27:15 +0400

    Усложненное готово

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-const getData = (url) => {
-	fetch(url)
-		.then((response) => response.json())
-		.catch((error) => console.log(error))
-		.then((data) => {
+const getData = (url, method) => {
+	let xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.responseType = "json";
+	xhr.send();
+	xhr.addEventListener("load", (e) => {
+		if (xhr.status != 200) {
+			console.log(`Что-то пошло не так, ошибка: ${xhr.status} - ${xhr.statusText}`);
+		} else {
 			sendData({
-				url: "https://jsonplaceholder.typicode.com/posts",
-				file: JSON.stringify(data),
 				method: "POST",
-				header: {
-					'Content-type': 'application/json; charset=UTF-8'
-				}
+				data: xhr.response,
+				url: "https://jsonplaceholder.typicode.com/posts"
 			});
-		})
+		}
+	});
 };
 
 const sendData = ({
+	data,
 	url,
-	method,
-	header,
-	file
+	method
 }) => {
-	fetch(url, {
-			method: method,
-			headers: header,
-			body: file
-		})
-		.then((response) => response.json())
-		.catch((error) => console.log(error))
-		.then((data) => console.log(data));
+	let xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+	xhr.send(JSON.stringify(data));
+	xhr.addEventListener("load", (e) => {
+		if (xhr.status != 201) {
+			console.log(`Ошибка: ${xhr.status} - ${xhr.statusText}`);
+		} else {
+			console.log(xhr.response);
+		}
+	});
 };
 
-getData("./db.json");
+getData("./db.json", "GET");
